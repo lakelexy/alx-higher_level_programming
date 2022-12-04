27cd777d51bd434e259bf4adf9da15dc4b41360d
lakelexy <alao8380@yahoo.com>
2022-12-04 13:26:02 +0000

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
 
-_c(my_string):
-    for i in range(len(my_string) - 2):
-        if (my_string[i] == 'c' or my_string[i] == 'C'):
-            my_string = my_string[:i] + "" + my_string[i+1:]
-    return (my_string)
+def no_c(my_string):
+    chars = len(my_string)
+    buf = ""
+    for i in range(0, chars):
+        if my_string[i] == "c" or my_string[i] == "C":
+            continue
+        buf = buf + my_string[i]
+    return buf
