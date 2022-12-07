d9d03aa8fec440713740a40343e4d07d39cbf7d9
lakelexy <alao8380@yahoo.com>
2022-12-07 14:05:02 +0000

    task1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-
-    
-    for i in range(0, len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-
+    new_list = list(my_list)
+    j = 0
+    for i in my_list:
+        if i == search:
+            new_list[j] = replace
+        j += 1
     return new_list
