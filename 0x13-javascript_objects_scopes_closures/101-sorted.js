685e7a530577d190773474b3137f311b8eb49663
lakelexy <alao8380@yahoo.com>
2023-03-14 14:45:14 +0000

    task12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* 
+/*
  * Script imports a dictionary of occurrences by user id
  * and computes a dictionary of user ids by occurrence.
  */
@@ -7,9 +7,9 @@
 const { dict } = require('./101-data.js');
 const Dictn = {};
 for (const N in dict) {
-    if (Dictn[dict[N]] === undefined) {
-	Dictn[dict[N]] = [];
-    }
-    Dictn[dict[N]].push(N);
+  if (Dictn[dict[N]] === undefined) {
+    Dictn[dict[N]] = [];
+  }
+  Dictn[dict[N]].push(N);
 }
 console.log(Dictn);
