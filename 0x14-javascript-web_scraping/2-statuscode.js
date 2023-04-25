73ee09d997fc6fdc15227ff2e2d6088dbedf401a
lakelexy <alao8380@yahoo.com>
2023-04-25 15:21:20 +0000

    task2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, (err, res, body) => {
-	  if (err) console.error(err);
-	  console.log('code:', res.statusCode);
+  if (err) console.error(err);
+  console.log('code:', res.statusCode);
 });
