b49b785ff7d94846a22639f5ea4370968be97598
lakelexy <alao8380@yahoo.com>
2023-04-25 15:07:18 +0000

    task2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// printing the status code of a GET request
+
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, res, body) => {
+	  if (err) console.error(err);
+	  console.log('code:', res.statusCode);
+});
