e54e811c9b70458100bd6e2d2ce1a96435edfe65
lakelexy <alao8380@yahoo.com>
2023-04-25 21:40:51 +0000

    task8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+
+const r = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+r.get(url, async (err, res, body) => {
+  if (err) console.log(err);
+  else {
+    for (const character of JSON.parse(body).characters) {
+      const name = await new Promise((resolve, reject) => {
+        r.get(character, (err, res, body) => {
+          if (err) reject(err);
+          else resolve(JSON.parse(body).name);
+        });
+      });
+      console.log(name);
+    }
   }
 });
-
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  });
-}
