49fc8444816b38c314290b1afc71998809727402
lakelexy <alao8380@yahoo.com>
2023-05-04 14:05:37 +0000

    task12

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,9 @@
+const url = 'https://www.fourtonfish.com/hellosalut/?lang=';
+
+$(this).ready(function () {
+  $('INPUT#btn_translate').on('click', function () {
+    $.getJSON(url + $('INPUT#language_code').val(), function (data) {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+});
